4435fa4983cc1a860f4ea57c3b4406968516d976
annasylvester <annaleesylvester@gmail.com>
2019-02-23 12:59:16 -0500

    Working ish

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,47 +3,44 @@ var matchesData = require("../data/matches");
 
 
 module.exports = function (app) {
-    // Get current data for matches list
-    app.get("/api/matches", function (req, res) {
-        res.json(matchesData);
-    })
+	// Get current data for matches list
+	app.get("/api/matches", function (req, res) {
+		res.json(matchesData);
+	})
 
-// Post new profile
-    app.post("/api/matches", function (req, res) {
-        console.log("posting new profile...");
-        let previousMatchScores = req.body.scores;
-        let scoresArray = [];
-        let bestMatch = 0;
+	// Post new profile
+	app.post("/api/matches", function (req, res) {
+		console.log("posting new profile...");
 
-        // gather data from all potential matches
-        for (let i=0; i<matchesData.length; i++){
-            let scoresDiff = 0;
-            for (let j=0; i<previousMatchScores.length; j++){
-                console.log("before parsing")
-                // issue right here
-                scoresDiff += (Math.abs(parseInt(matchesData[i].scores[j]) - parseInt(previousMatchScores[j])));
-                console.log("after parsing")
-            }
-            scoresArray.push(scoresDiff);
-        }
-        console.log("scores sent to arrAY")
-        
+		var newFriendScores = req.body.scores;
+		var scoresArray = [];
+		var friendCount = 0;
+		var bestMatch = 0;
 
-        // find best match
-        for (let i=0; i<scoresArray.length; i++){
-            if(scoresArray[i] <= scoresArray[bestMatch]){
-                bestMatch = i;
-            }
-        }
-        console.log("finding best match....")
+		//runs through all current friends in list
+		for (var i = 0; i < matchesData.length; i++) {
+			var scoresDiff = 0;
+			//run through scores to compare friends
+			for (var j = 0; j < newFriendScores.length; j++) {
+				scoresDiff += (Math.abs(parseInt(matchesData[i].scores[j]) - parseInt(newFriendScores[j])));
+			}
 
-        // return best match
-        let usersMatch = matchesData[bestMatch];
-        res.json(usersMatch);
+			//push results into scoresArray
+			scoresArray.push(scoresDiff);
+		}
 
-        // push new submission into matches data
-        matchesData.push(req.body);
+		//after all friends are compared, find best match
+		for (var i = 0; i < scoresArray.length; i++) {
+			if (scoresArray[i] <= scoresArray[bestMatch]) {
+				bestMatch = i;
+			}
+		}
 
-    })
+		//return bestMatch data
+		var bff = matchesData[bestMatch];
+		res.json(bff);
 
-}
+		//pushes new submission into the friendsList array
+		matchesData.push(req.body);
+	});
+};
