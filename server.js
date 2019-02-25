a5358dcd0c2cd70efcae527c0f47688cb5d06aae
annasylvester <annaleesylvester@gmail.com>
2019-02-25 00:14:00 -0500

    Modal testing

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'app', 'public')));
 
 //ROUTER
 require('./app/routing/apiRoutes')(app); 
