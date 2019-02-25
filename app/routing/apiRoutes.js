0fdc3aa22ccccc7e4ce86fd8924c7dbcf38c4e4f
annasylvester <annaleesylvester@gmail.com>
2019-02-25 02:07:34 -0500

    Fix bugs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,5 @@
 // Load Survey Data
-const matchesData = require("../data/matches");
+var matchesData = require("../data/matches");
 
 
 module.exports = function (app) {
@@ -10,37 +10,33 @@ module.exports = function (app) {
 
 	// Post new profile
 	app.post("/api/matches", function (req, res) {
-		console.log("posting new profile...");
 
-		let newMatchScores = req.body.scores;
-		let scoresArray = [];
-		let bestMatch = 0;
+		var newMatchData = req.body.scores;
+		var scoresArray = [];
+		var bestMatch = 0;
 
 		//runs through all current friends in list
-		for (let i = 0; i < newMatchScores.length; i++) {
-			let scoresDiff = 0;
+		for (var i = 0; i < matchesData.length; i++) {
+			var scoresDiff = 0;
 			//run through scores to compare friends
-			for (let j = 0; j < newMatchScores.length; j++) {
-				scoresDiff += (Math.abs(parseInt(matchesData[i].scores[j]) - parseInt(newMatchScores[j])));
+			for (var j = 0; j < newMatchData.length; j++) {
+				scoresDiff += (Math.abs(parseInt(matchesData[i].scores[j]) - parseInt(newMatchData[j])));
 			}
 
 			//push results into scoresArray
 			scoresArray.push(scoresDiff);
-			console.log("Results pushed to array");
 		}
 
 		//after all friends are compared, find best match
-		for (let i = 0; i < scoresArray.length; i++) {
+		for (var i = 0; i < scoresArray.length; i++) {
 			if (scoresArray[i] <= scoresArray[bestMatch]) {
 				bestMatch = i;
 			}
 		}
-		console.log("finding best match");
 
 		//return bestMatch data
-		let theMatch = matchesData[bestMatch];
+		var theMatch = matchesData[bestMatch];
 		res.json(theMatch);
-		console.log("match sent")
 
 		//pushes new submission into the friendsList array
 		matchesData.push(req.body);
